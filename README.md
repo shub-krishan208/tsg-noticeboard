ce2a2ef8fcc86422e93ff442a9c8246cdf75512f
shub-kroshan208 <shubhamkrishan999@gmail.com>
2025-08-06 12:18:40 +0530

    wrote first api call

--- README.md
+++ README.md
@@ -12,3 +12,9 @@ make the .env file from the **example.env** provided and provide the necessary i
 run `docker compose up -d --build` in the cloned repo directory and it'd work well.
 
 Backend can be accessed on port 5000 and frontend at 3000. (if backend port is unavailable check docker-compose.yaml and ./backend/Dockerfile for a new port setup.)
+
+## Checklist before deployment
+
+- Note that .env has proper credentials
+- admins are added properly in a separate object
+- backend port is not exposed anymore
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,47 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const viewNotice = (id) => {
+    // try {
+    //   const res = await fetch("/api/notices");
+    //   if (!res.ok) {
+    //     throw new Error("Failed to load the notices");
+    //   }
 
+    //   const notices = await res.json();
+    //   console.log(notices);
+    // } catch (err) {
+    //   console.log("Can't load notices: ", err);
+    // }
+    fetch("/api/notices")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Failed to load notices");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
   return (
     <>
-      <div className="flex bg-amber-100 text-[#000000]">Hellowww</div>
+      <div className="bg-slate-900 h-svh w-svw">
+        <div className="flex justify-center p-2 bg-slate-950 text-amber-100 text-3xl">
+          <strong>Testing Noticeboard stuff</strong>
+        </div>
+        <div className="my-2 h-3/4 w-svw border-2 border-amber-300 flex p-5">
+          <button
+            onClick={viewNotice(1)}
+            className="text-center border-2 p-0.5 border-amber-300 h-[30px] w-[100px] text-amber-50"
+          >
+            First notice
+          </button>
+        </div>
+      </div>
     </>
   );
 }
