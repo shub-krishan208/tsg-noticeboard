17bef11ec7013f82ac69c50a275b81cb09bd1d4f
shub-kroshan208 <shubhamkrishan999@gmail.com>
2025-08-06 10:29:08 +0530

    nginx configured, tailwindcss setup complete

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="flex bg-amber-100 text-[#000000]">Hellowww</div>
+    </>
+  );
+}
+
+export default App;
