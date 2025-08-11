70f963946e437417e8e3210aa384ea5df80e06db
shub-kroshan208 <shubhamkrishan999@gmail.com>
2025-08-11 18:00:35 +0530

    fixed vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { ViteFonts } from "vite-plugin-fonts";
+// import { ViteFonts } from "vite-plugin-fonts";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    ViteFonts({
-      custom: {
-        families: [
-          {
-            name: "Lato",
-            local: "Lato",
-            // Explicitly list only the fonts you want
-            src: ["./src/assets/fonts/Lato/*.ttf"],
-          },
-        ],
-        display: "swap", // ensures text shows immediately with fallback font
-      },
-    }),
+    // ViteFonts({
+    //   custom: {
+    //     families: [
+    //       {
+    //         name: "Lato",
+    //         local: "Lato",
+    //         // Explicitly list only the fonts you want
+    //         src: ["./src/assets/fonts/Lato/*.ttf"],
+    //       },
+    //     ],
+    //     display: "swap", // ensures text shows immediately with fallback font
+    //   },
+    // }),
   ],
 });
