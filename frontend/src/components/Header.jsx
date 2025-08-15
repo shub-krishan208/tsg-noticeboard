b6ed5ff0db5d15b573419a5ccf1c9915253e7976
shub-kroshan208 <shubhamkrishan999@gmail.com>
2025-08-15 11:53:19 +0530

    link to tsg main page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,10 +3,16 @@ import gymkhanaLogo from "../assets/gymkhanaLogo.png";
 const Header = ({ onLoginClick }) => {
   return (
     <header className="bg-black flex justify-between items-center">
-      <div className="flex items-center gap-1 px-8 py-4">
-        <img src={gymkhanaLogo} alt="Gymkhana Logo" className="w-7 h-7" />
-        <span className="text-white text-lg">TSG</span>
-      </div>
+      <a
+        href="https://gymkhana.iitkgp.ac.in/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="flex items-center gap-1 px-8 py-4">
+          <img src={gymkhanaLogo} alt="Gymkhana Logo" className="w-7 h-7" />
+          <span className="text-white text-lg">TSG</span>
+        </div>
+      </a>
       <div className="flex items-center gap-8 text-white px-10 py-4 text-base"></div>
     </header>
   );
